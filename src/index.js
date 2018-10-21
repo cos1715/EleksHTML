3c8da8f1b1d52671370694428c34be211f33d93c
Taras <tsamoilenko97@gmail.com>
2018-10-22 02:32:20 +0300

    responsive finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-/* This is just example */
+; (function () {
+    const $dropdownMenu = document.getElementById('drop-menu');
+    const $arrowSvg = document.getElementById('chevron-circle-down');
+    const $asideMenu = document.getElementById('aside-toggle');
+    const $asideArrowSvg = document.getElementById('svg-toggle');
+    const $dropContent = document.getElementById('drop-content');
 
-debugger;
-const foo = 1;
+    function addDropdownAnimation() {
+        $arrowSvg.classList.add('arrow-rotation');
+        $dropContent.classList.add('dropdown-animation');
+        $dropdownMenu.removeEventListener('click', addDropdownAnimation);
+    };
+    function addHideAnimation() {
+        $asideArrowSvg.classList.add('svg-aside-toggle');
+        $asideMenu.removeEventListener('click', addHideAnimation);
+    }
+    $dropdownMenu.addEventListener('click', addDropdownAnimation);
+    $asideMenu.addEventListener('click', addHideAnimation);
+})();
